61ad8223fab03d9ddaf28bfaa3c50cddd5280c83
eddieim <eisaiahmo22@gmail.com>
2024-05-23 17:02:27 -0600

    added all the required questions for the readme

--- index.js
+++ index.js
@@ -3,3 +3,66 @@ const fs = require ("fs");
 const inquirer = require ("inquirer");
 const path = require ("path");
 const generateMarkdown = require ("./utils/generateMardown")
+
+//Creates an array of questions for the readme that the user inputs
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Write the name of your project.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe the purpose and functionality of your progject.",
+    },
+    {
+        type: "input",
+        name: "scrennshot",
+        message: "Input a relative path to display your screeenshot image.",
+    },
+    {
+        type: "input",
+        name: "link",
+        message: "Select a license if applicable.",
+        choices: ["MIT", "APACHE 2.0", "Boost 1.0", "MPL 2.0", "BSD2", "BSD3", "none"],
+        //choices gives the user the option two pick form the selected above
+    },
+    {
+        type: "input",
+        name: "require",
+        message: "List any dependencies used.",
+    },
+    {
+        type: "input",
+        name: "features",
+        message: "List any special features your app provides.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Include languages or technologies used in the project.",
+    },
+    {
+        type: "input",
+        name: "creator",
+        message: "Input your Github username.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Provide a valid email address.",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please list any contributors, tutorial links, or Github usernames.",
+        default: "",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide a walkthrough of required test if applicable.",
+    },
+];
